3a7772ecfd8207f69bd5dc6460cbeb4b98409789
henadalGH <hrnnnadal@gmail.com>
2024-09-26 11:30:25 -0300

    Cuarto commit de la rama priact (Agrega logica al boton salir)

--- app/src/main/java/com/example/ej10_2/MainActivity.java
+++ app/src/main/java/com/example/ej10_2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ej10_2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        activarBotones();
+    }
+
+    private void activarBotones()
+    {
+        Button btnSalir = (Button) findViewById(R.id.btnSalir);
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
         });
     }
 }
